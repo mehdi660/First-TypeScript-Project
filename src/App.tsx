88f7b4da87de3722fca76a65e5dcbc0c228f8c71
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-07-17 18:09:16 +0200

    learning type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+
 function App() {
-  return <h1>Typescript</h1>;
+  const [prix, setPrix] = useState(1);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={prix}
+        onChange={(e) => setPrix(e.target.value)}
+      />
+      <h1>{prix * 2} euros</h1>
+    </div>
+  );
 }
 
 export default App;
+
+// Typage explicite
+
+// Typage Implicite ( à privilégier)
