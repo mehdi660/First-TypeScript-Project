2cd3f1ebe3dffd6f44c4a7d9299e2584f12dfb12
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-07-17 19:52:46 +0200

    learing UnionType

--- src/components/Fruit.tsx
+++ src/components/Fruit.tsx
@@ -3,9 +3,14 @@ import { FruitType } from "../typescript/FruitType";
 type FruitProps = {
   fruitInfo: FruitType;
   onClick: () => void;
+  backgroundColor?: string;
 };
 
-export default function Fruit({ fruitInfo, onClick }: FruitProps) {
+export default function Fruit({
+  fruitInfo,
+  onClick,
+  backgroundColor,
+}: FruitProps) {
   // state
   // const fruitInfo = props.fruitInfo;
   // const onFruitDelete = props.onFruitDelete;
@@ -15,7 +20,10 @@ export default function Fruit({ fruitInfo, onClick }: FruitProps) {
   // affichage (render)
   return (
     <li>
-      {fruitInfo.nom} <button onClick={onClick}>X</button>
+      {fruitInfo.nom}{" "}
+      <button style={{ background: backgroundColor }} onClick={onClick}>
+        X
+      </button>
     </li>
   );
 }
